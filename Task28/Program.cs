f6cbdcf52b1368a42bce384758539a461647d6a2
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-05-19 21:42:50 +0300

    task28

---
+++ Task28/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая принимает на вход число N
+//   и выдаёт произведение чисел от 1 до N. 
+//   4 -> 24 5 -> 120
+
+Console.WriteLine("Введите число ->");
+int n = int.Parse(Console.ReadLine());
+
+int GetComNumbers(int number)
+{
+    int composition = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        composition = composition * i;
+    }
+    
+    return composition;
+}
+
+
+
+int ComResult = GetComNumbers (n);
+
+Console.WriteLine($"Произведение чисел от 1 до {n} равна {ComResult}");
