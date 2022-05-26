56e009321ffc543c9498bb0c54c83f1200eebdc4
KseniyaLK <103847027+KseniyaLK@users.noreply.github.com>
2022-05-26 13:26:40 +0300

    task29_1

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,4 +3,33 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-int [] array = new int[]
+
+Console.Write("Задайте разер массива: ");
+int size = int.Parse (Console.ReadLine());
+// Console.WriteLine("Введите элементы массива через пробел: ");
+// int [] numArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+// Console.WriteLine(string.Join(' ', numArray));
+
+
+int [] array = new int [size];
+
+ void FillArray (int [] array)
+ {
+     for (int i = 0; i < array.Length; i++)
+     {
+         array[i] = new Random().Next(1, 99);
+     }
+ }
+
+ void PrintArray (int [] array)
+ {
+     for (int i = 0; i < array.Length; i++)
+     {
+         Console.Write(array[i] +"\t");
+     }
+ }
+ 
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(" ");
